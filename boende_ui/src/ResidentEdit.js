52d4247182d1d5d2cdd58c102befb24072ccc055
torarvill <arvill@kth.se>
2021-03-05 13:06:01 +0100

    Split call-button into two classes so I can use the same button for other purposes, also added edit relative file, but it's currently non functional

---
+++ boende_ui/src/ResidentEdit.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import backarrow from './back-arrow.png';
+
+function Resident(){
+    let residents = ["Marie Curie","Pierre Curie"];
+    return (
+        <div>
+            <div className="button"></div>
+            <div className="flexbox">
+            <div className="flexbox topAligned">
+                <Link to = "/"><img src={backarrow} /></Link>
+            </div>
+            <div className="flexbox topAligned">
+                <h1 className="extra-large-text center">Boende</h1>
+                <div className="scroll-container">
+                    <div className="scroll-header">
+                        <p className="center">Klicka på den boende ni vill redigera</p>
+                    </div>
+                    <div className="scroll-outer largeScroll">
+                        <div className="scroll-inner">
+                            {
+                                residents.map(resident =>
+                                    (
+                                        <Link to ="/Relative"><p className="scroll-row"><span className="name-plate">{resident}</span></p></Link>
+                                    ))
+                            }
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="flexbox columnMedium bottomAligned">
+                <h1 class="large-add-button white-text extra-large-text centerDivVertical">Påbörja samtal <br /> &#9742;</h1>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Resident;
--- boende_ui/src/Start.js
+++ boende_ui/src/Start.js
@@ -8,16 +8,16 @@ function Start() {
             <h1 class='headerstyle'>doGood</h1>
             <link rel="stylesheet" href="styleOne.css" />
         </div>
-    
-    
+
+
         <div className="center">
             <Link to="/Resident" ><button class="button button1" id='startaSamtal' >Starta Samtal</button></Link>
             <br></br>
             <br></br>
-            <button class="button button2" id='redigera'>Redigera</button>
+            <Link to="/ResidentEdit" ><button class="button button2" id='redigera'>Redigera</button></Link>
         </div>
     </div>
-    
+
     );
 }
 
