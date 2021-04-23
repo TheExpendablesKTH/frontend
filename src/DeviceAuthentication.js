4b53f1bc630484e9c2f02c325bc50c6d66eae24b
torarvill <arvill@kth.se>
2021-04-23 16:52:59 +0200

    Improved DeviceAuth, but it still has problems communicating with backend and is therefore set to autoskip

--- src/DeviceAuthentication.js
+++ src/DeviceAuthentication.js
@@ -10,21 +10,35 @@ function DeviceAuthentication(){
     const [loading, setLoading] = useState(true);
     const [hasToken, setHasToken] = useState(false);
     const inputRef = useRef();
-    const [password, setPassword]=useState("");
+    const [password, setPassword]=useState(""); //DEBUG!!
+    const api_url = "http://master.api.dd1369-meetings.com";
+
 
     useEffect(() => {
         const check = async () =>{
                 if(localStorage.getItem("DeviceToken")!==null){
-                  setHasToken(false);
+                  setHasToken(true);
                   setLoading(false);
                 }else{
-                  setHasToken(false);
+                  setHasToken(true); //should be false, but done like this to auto-skip until everything works
                   setLoading(false);
                 }
             };
             check();
         },[loading]);
 
+      function userClickedAuthenticate(newPassword){
+        const fetchAndStoreData = async () => {
+            storeToken(await axios(api_url+'/authenticate/device',{'passphrase':newPassword},{headers:{'Content-Type':'application/json'}}));
+
+        };
+        fetchAndStoreData();
+        }
+
+      function storeToken(newToken){
+          localStorage.setItem("DeviceToken", newToken);
+        setLoading(true);
+      }
     /*    useEffect(() => inputRef.current.focus(), [inputRef]); */ //copied from chime-poc
 
 
@@ -37,8 +51,8 @@ function DeviceAuthentication(){
         <input
             ref={inputRef}
         />
-          <button onClick={() => setPassword(inputRef.current.value)}>Authenticate</button>
-            <p>DEBUG! Password entered DEBUG!{password}</p>
+          <button onClick={() => userClickedAuthenticate(inputRef.current.value)}>Authenticate</button>
+            <p>DEBUG! Password:{password}</p>
         </div>
       }
       </div>
