311ef0bf6f2340a0ea659dd41c1e9a7d2674a3b8
Anton-Bolenius <76431849+Anton-Bolenius@users.noreply.github.com>
2021-04-27 14:54:48 +0200

    fixed connection to api

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,31 +1,29 @@
 import React, {useState, useEffect} from "react";
 import {Link, Redirect} from 'react-router-dom';
 import backarrow from './back-arrow.png';
-
+import axios from 'axios';
 function LoginForm() {
-    const [loggedIn, setLoggedIn] = useState();
-    const [failed, setFailed] = useState();
-    const [adminToken, setAdminToken] = useState();
-    useEffect(() => {
-        setLoggedIn(false);
-        setFailed(false);
-        },[]);
-    const handleSubmit = (event) => {
-	const username = document.getElementById("username").value;
-	const password = document.getElementById("password").value;
-        //change the condition in the if statement
-        if ((username === "hehe") && (password === "yikes")) {
-          setFailed(false);
-          setAdminToken("some_token"); //get from request
-          setLoggedIn(true);
-        } else {
-          setFailed(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [failed, setFailed] = useState(false);
+    const [admin_token, setAdminToken] = useState(null);
+    const api_url = "http://master.api.dd1369-meetings.com/";
+    const device_token = "eyJhbGciOiJIUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAABXMQQ5AMBAAwK_IniuxqzT2HU5EpNGN9EClLRfxd9wnc4NPCRhcWENwoMDbDIwtdg3VqI2CM0nc7SY_kssvMi9RbBY328xUEZaVLsn0iFwTkxm-5JC4fa8P-3-PqAqtimaC5wXl2A9qbwAAAA.7WGQGnYoQu1y6PhLXqIG5xg5fVhK8MSvVRJaBBP2YlY";
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const result = await axios.post(api_url + "/authenticate/admin",{'username':username,'password':password},{headers:{'Content-Type':'application/json','Authorization':device_token}}).catch(err =>{
+            setFailed(true);
+        });
+        if (!failed){
+            setAdminToken(result.data.token);            
+            setLoggedIn(true);            
         }
-        event.preventDefault();
     };
     return (
     <div>
-    {loggedIn && <Redirect to={{pathname:"/ResidentEdit", adminToken:adminToken}}/>}
+    {loggedIn && <Redirect to={{pathname:"/ResidentEdit", admin_token:admin_token}}/>}
     {!loggedIn && <div><ul class="breadcrumb br2">
     <li> Loggar in som admin </li>
     </ul>
@@ -63,3 +61,4 @@ function LoginForm() {
 }
 
 export default LoginForm;
+
