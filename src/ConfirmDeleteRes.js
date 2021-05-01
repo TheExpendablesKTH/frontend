37e9a20c4a02f0a8a82d93813b46141cbf8c81d8
amielien <45800515+amielien@users.noreply.github.com>
2021-05-01 11:55:39 +0200

    redirects to feedback page after deleting resident

--- src/ConfirmDeleteRes.js
+++ src/ConfirmDeleteRes.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, {useState} from "react";
+import {Link, Redirect} from 'react-router-dom';
 import backarrow from './back-arrow.png';
 import axios from 'axios';
 import { findGetParameter } from './Relative';
@@ -8,20 +8,23 @@ function ConfirmDeleteRes(){
     const resident_name = findGetParameter("resident_name");
     const api_url = "http://master.api.dd1369-meetings.com";
     const admin_token = localStorage.getItem("admin_token");
+    const [deleted, setDeleted] = useState(false);
     const deleteResident = async (e) => {
         e.preventDefault();    
-        await axios.delete(api_url+"/residents/"+resident_id,{headers: {'Content-Type':'application/json', 'Authorization':admin_token}});                      
+        await axios.delete(api_url+"/residents/"+resident_id,{headers: {'Content-Type':'application/json', 'Authorization':admin_token}});
+        setDeleted(true);
     };
     return(
-        <div>        
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>            
+        <div>  
+            {deleted && <Redirect to={{pathname:"/DeleteResFeedback", resident_name:resident_name}}/>}      
+            {!deleted && <div><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>            
             
             <div className="center">
             <p className="headerstyle">Bekräfta borttagning av boende</p>
                 <Link to={"/ResidentEditor?resident_id="+resident_id+"&resident_name="+resident_name}><img src={backarrow} alt="backarrow" /></Link>
                         <br></br><br></br>
                         <button onClick={deleteResident} class="button button2" id='raderaAnhorig' >Jag vill radera boende: {resident_name}</button>
-            </div>
+            </div></div>}
         </div>
     )
 }
