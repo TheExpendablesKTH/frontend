32c75a301f3b3d858eeefdd4b1c2c4e3a771e743
amielien <45800515+amielien@users.noreply.github.com>
2021-05-01 13:36:22 +0200

    added feedback page for adding resident

---
+++ src/AddResFeedback.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import backarrow from './back-arrow.png';
+function AddResFeedback(props){
+    return(
+        <div>    
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>            
+            
+            <div className="center">
+            <p className="headerstyle">Du har skapat en ny boende: {props.location.resident_name}</p>
+                <Link to ={"/ResidentEdit"}><img src={backarrow} alt="backarrow" /></Link>
+                        <br></br><br></br>
+                        <Link to ={"/ResidentEdit"}><button class="button button1">OK</button></Link>
+            </div>
+        </div>
+    )
+}
+
+export default AddResFeedback;
