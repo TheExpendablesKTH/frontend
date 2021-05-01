235f0d2c99357bc660c363115f475a38b2a79256
amielien <45800515+amielien@users.noreply.github.com>
2021-05-01 13:50:48 +0200

    redirects to feedback page after adding relative

--- src/RelativeAdd.js
+++ src/RelativeAdd.js
@@ -1,21 +1,25 @@
-import React, { useRef } from "react";
-import {Link} from 'react-router-dom';
+import React, {useState, useRef } from "react";
+import {Link, Redirect} from 'react-router-dom';
 import backarrow from './back-arrow.png';
 import { findGetParameter } from "./Relative";
 import axios from 'axios';
+
 function RelativeAdd() {
     const resident_name = findGetParameter("resident_name");
     const resident_id = findGetParameter("resident_id");
     const nameToSave = useRef(null);
     const phoneToSave = useRef(null);
     const api_url = "http://master.api.dd1369-meetings.com/residents/"+resident_id+"/relatives";
+    const [added, setAdded] = useState(false);
     const saveRelative = async (e) => {
         e.preventDefault();            
         await axios.post(api_url,{'name':nameToSave.current.value,'phone':phoneToSave.current.value},{headers: {'Content-Type':'application/json', 'Authorization':localStorage.getItem("admin_token")}});            
+        setAdded(true);
     };
 
     return (
-    <div><ul class="breadcrumb br2">
+    <div>{added && <Redirect to={{pathname:"/AddRelFeedback", relative_name:nameToSave.current.value, resident_id:resident_id, resident_name:resident_name}}/>}  
+    {!added && <div><ul class="breadcrumb br2">
     <li>Redigera: Lägger till anhörig till {resident_name}</li>
     </ul>
         <div className="upper-left">
@@ -41,7 +45,7 @@ function RelativeAdd() {
             <br></br>
             <input type="submit" value="Spara" />
         </form>
-        </div>
+        </div></div>}
     </div>
 
     );
