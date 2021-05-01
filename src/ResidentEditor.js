e29cbdc7a18727b031a422e948594ed79e3db9cc
amielien <45800515+amielien@users.noreply.github.com>
2021-05-01 13:15:34 +0200

    redirects to feedback page after updating resident
    note: actually updating a resident still lacks support. tested using placeholder function without any actual requests, then commented the form away again to avoid misleading the user (as the functionality doesnt actually exist). however, as soon as the appropriate requests are added and the form is un-hidden the code should redirect and give you feedback as is appropriate (in other words the rest of the code is done, its just the updating itself that is missing)

--- src/ResidentEditor.js
+++ src/ResidentEditor.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef} from "react";
-import { Link } from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import backarrow from './back-arrow.png';
 import axios from 'axios';
 import { findGetParameter } from './Relative';
@@ -7,17 +7,26 @@ import { findGetParameter } from './Relative';
 function ResidentEditor() {
     const nameToSave = useRef(null);
     const [loading, setLoading] = useState(false);
+    const [updated, setUpdated] = useState(false);
     const resident_id = findGetParameter("resident_id");
     const resident_name = findGetParameter("resident_name");
     const relative_id = findGetParameter("relative_id");
     const relative_name = findGetParameter("relative_name");
     const api_url = "http://master.api.dd1369-meetings.com/";
     
-    
+    // Update to be async, see RelativeEditor.js
+    const updateResident = (e) => {
+            e.preventDefault();
+            setLoading(true);        
+            // Add relevant requests, see RelativeEditor.js
+            setLoading(false);
+            setUpdated(true);
+        };
 
 
     return (
-        <div><ul class="breadcrumb br2">
+        <div>{updated && <Redirect to={{pathname:"/UpdateResFeedback", resident_id:resident_id, resident_name:resident_name}}/>}        
+            {!updated && <div><ul class="breadcrumb br2">
         <li>Redigera: Redigerar boende {resident_name}</li>
         </ul>
             <div className="upper-left">
@@ -37,22 +46,22 @@ function ResidentEditor() {
                         </div>
                         <div className="center">
                             {/* The below commented out section is supposed to be used for updating a resident's name but at the moment of writing the API does not support updating without deleting the resident and all it's relatives */}
-                             {/* <form onSubmit={updateResident}>
+                             {/*<form onSubmit={updateResident}>
                                 <label>                                
                                     <p class="form-headline">Namn:</p>
                                     <br></br>
-                                    <input type="text" name="name"  ref={nameToSave}/>
+                                    <input type="text" name="name" ref={nameToSave} required/>
                                 </label>
                             <br></br>
                             <input type="submit" value="Spara"/>
-                            </form> */}
+                            </form>*/}
                             <br></br>
                             <br></br>
                             <Link to={"/ConfirmDeleteRes?resident_id="+resident_id+"&resident_name="+resident_name}><button class="button button2" id='raderaAnhorig' >Radera {resident_name}</button></Link>
                         </div>
                     </div>
 
-                )}
+                )}</div>}
         </div>
 
     );
